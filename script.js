92efb79ec21f90d1b7ef634150c48c122d0e6fa4
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-28 14:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    const table = document.getElementById("sampleTable");
+
+    // create new row at top (index 0)
+    const row = table.insertRow(0);
+
+    // create two cells
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    // add content
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
 }
